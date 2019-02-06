eeee91af2ef98cfb004b20c354340777a56293cb
Isaac Smith <isaac.smitty@gmail.com>
2019-02-06 16:28:49 +0000

    initial commit concats to single string

---
+++ character-counting.js
@@ -0,0 +1,38 @@
+var counting = function(word) {
+
+  var input = '';
+  input = word.split(' ');
+  input = input.join('');
+
+  console.log(input);
+
+  var output = {};
+
+for (var i = 0; i < word.length; i++) {
+
+
+}
+
+
+
+}
+
+
+
+console.log(counting("lighthouse in the house"));
+
+
+
+
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
